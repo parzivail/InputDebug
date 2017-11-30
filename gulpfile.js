a745f9621b736d3f603fbde842dafcdd1da1d8e6
Colby Newman <colby.newman2000@gmail.com>
2017-11-29 22:36:20 -0500

    gamepad start

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,51 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat'),
-	pump = require('pump');
+	pump = require('pump'),
+	connect = require('gulp-connect');
 
-var sassSources = ['web/scss/style.scss'];
+require('dotenv').config();
+
+var jsSources = ['web/js/*.js'],
+	sassSources = ['web/scss/style.scss'],
+	htmlSources = ['web/**/*.html'];
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'static',
+		livereload: true
+	});
+});
 
 gulp.task('sass', function (cb) {
 	pump([
 			gulp.src(sassSources),
 			sass(),
 			concat('style.css'),
-			gulp.dest("web/css")
+			gulp.dest("static/css"),
+			connect.reload()
 		],
 		cb);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('js', function (cb) {
+	pump([
+			gulp.src(jsSources),
+			uglify(),
+			gulp.dest("static/js"),
+			connect.reload()
+		],
+		cb);
+});
+
+gulp.task('html', function () {
+	connect.reload();
+});
+
+gulp.task('watch', function () {
+	gulp.watch(jsSources, ['js']);
+	gulp.watch(sassSources, ['sass']);
+	gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('default', ['sass', 'js', 'connect', 'watch']);
